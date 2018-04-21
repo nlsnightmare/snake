c56b732a795025d57c26fa132fa06fd96826e27a
Nlsnightmare < >
2018-04-21 17:27:55 +0300

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/code.js',
+    mode: 'development',
+    module: {
+	rules: [
+	    {
+		test: /\.js$/,
+		exclude: '/node_modules/',
+		use: ['babel-loader']
+	    }
+	]
+    },
+    output:{
+	filename: 'bundle.js',
+	path: path.resolve(__dirname,'dist')
+    },
+    devServer:{
+	overlay:true,
+	contentBase: path.join(__dirname,'dist'),
+	compress: true,
+	port:8001
+    },
+};
