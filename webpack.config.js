64150b25a07db30151a5262318d37e909484c21b
Νίκος Μαρουλάκης <nlsnightmare@gmail.com>
2019-06-04 22:22:52 +0300

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,22 @@ module.exports = {
     entry: './src/code.js',
     mode: 'development',
     module: {
-	rules: [
-	    {
-		test: /\.js$/,
-		exclude: '/node_modules/',
-		use: ['babel-loader']
-	    }
-	]
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                use: ['babel-loader']
+            }
+        ]
     },
     output:{
-	filename: 'bundle.js',
-	path: path.resolve(__dirname,'dist')
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'dist')
     },
     devServer:{
-	overlay:true,
-	contentBase: path.join(__dirname,'dist'),
-	compress: true,
-	port:8001
+        overlay:true,
+        contentBase: path.join(__dirname,'dist'),
+        compress: true,
+        port:8001
     },
 };
